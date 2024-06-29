45278c11e12f26435fd4ff7e337af40eaab05650
Mahtab Dastranj <mahtab_dastranj@yahoo.com>
2024-06-29 12:15:39 +0330

    last version before representation

--- Hand_pose_estimation.py
+++ Hand_pose_estimation.py
@@ -6,48 +6,37 @@ import os
 from matplotlib import pyplot as plt
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 
-with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
-    while cap.isOpened():
-        ret, frame = cap.read()
-
+# with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+#    while cap.isOpened():
+#        ret, frame = cap.read()
         # BGR to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
+#        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         # Flip on horizontal
-        image = cv2.flip(image, 1)
-
+#        image = cv2.flip(image, 1)
         # Set flag
-        image.flags.writeable = False
-
+#        image.flags.writeable = False
         # Detections
-        results = hands.process(image)
-
+#        results = hands.process(image)
         # Set flag to true
-        image.flags.writeable = True
-
+#        image.flags.writeable = True
         # RGB to BGR
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
+#        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         # Detections
-        print(results)
-
+#        print(results)
         # Rendering results
-        if results.multi_hand_landmarks:
-            for num, hand in enumerate(results.multi_hand_landmarks):
-                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
-                                          mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-                                          mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
-                                          )
-
-        cv2.imshow('Hand Tracking', image)
-
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-
-cap.release()
-cv2.destroyAllWindows()
+#        if results.multi_hand_landmarks:
+#            for num, hand in enumerate(results.multi_hand_landmarks):
+#                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
+#                                          mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+#                                          mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
+#                                          )
+#        cv2.imshow('Hand Tracking', image)
+#        if cv2.waitKey(10) & 0xFF == ord('q'):
+#            break
+# cap.release()
+# cv2.destroyAllWindows()
 # Output images
 path = r'E:\AUT\6TH\rehabilation\code\Muscle_Dystrophy\Output Results'
 if not os.path.exists(path) or not os.path.isdir(path):
